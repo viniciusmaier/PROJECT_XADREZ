c4ab71de9eb979682afe443e2524c9c9e77e20a8
vinicius maier <viniciusfiel1000@gmail.com>
2023-10-13 15:21:27 -0300

    add position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package src.application;
 
+import src.boardgame.Position;
+
 public class Program {
     public static void main(String[]args){
-        System.out.println("hello world");
+        Position p1 = new Position(1, 3);
+        System.out.println(p1);
     }
 }
